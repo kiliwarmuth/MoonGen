243aa8b3dd965240bf3ea632642f713ae6892271
Kili Warmuth <38520717+kiliwarmuth@users.noreply.github.com>
2024-04-03 19:58:38 +0200

    Update rss.lua
    small fix

--- examples/rss.lua
+++ examples/rss.lua
@@ -5,47 +5,43 @@ local memory = require "memory"
 local device = require "device"
 local stats  = require "stats"
 local log    = require "log"
+local mg     = require "moongen"
 
-local PKT_SIZE  = 60
-local NUM_FLOWS = 100
-
-function master(txPort, rxPort, rxQueues)
-	if not txPort or not rxPort then
-		log:info("Usage: txPort rxPort [rxQueues]")
-		return
-	end
-	rxQueues = rxQueues or 2
-	local txDev = device.config{port = txPort}
-	local rxDev = device.config{
-		port = rxPort,
-		rxQueues = rxQueues + 1,
-		rssQueues = rxQueues,
-		rssBaseQueue = 1 -- optional and defaults to 0
-	}
-	device.waitForLinks()
-	dpdk.launchLua("txSlave", txDev:getTxQueue(0))
-	for i = 1, rxQueues do
-		dpdk.launchLua("rxSlave", rxDev:getRxQueue(i))
-	end
-	dpdk.waitForSlaves()
+function configure(parser)
+	parser:description("Generates traffic, enables RSS and measures how much traffic is received per queue")
+	parser:argument("txPort", "Device to transmit from."):convert(tonumber)
+	parser:argument("rxPort", "Device to receive from."):convert(tonumber)
+	parser:option("-q --queues", "The number of queues to configure."):default(2):convert(tonumber)
+	parser:option("-s --size", "Packet size."):default(60):convert(tonumber)
+	parser:option("-f --flows", "The number of flows to generate."):default(100):convert(tonumber)
 end
 
-function txSlave(queue)
+function master(args)
+	local rxDev = device.config({port = args.rxPort, rxQueues = args.queues + 1, rssQueues = args.queues, rssBaseQueue = 1})
+	local txDev = device.config({port = args.txPort})
+	device.waitForLinks()
+	mg.startTask("txSlave", txDev:getTxQueue(0), args.size, args.flows)
+	for i = 1, args.queues do
+		mg.startTask("rxSlave", rxDev:getRxQueue(i))
+	end
+	mg.waitForTasks()
+end
+
+function txSlave(queue, size, flows)
 	local mempool = memory.createMemPool(function(buf)
 		buf:getUdpPacket():fill{
-			ethSrc = queue,
-			pktLength = PKT_SIZE
+			pktLength = size
 		}
 	end)
 	local bufs = mempool:bufArray()
 	local counter = 0
 	local txCtr = stats:newDevTxCounter(queue, "plain")
-	while dpdk.running() do
-		bufs:alloc(PKT_SIZE)
+	while mg.running() do
+		bufs:alloc(size)
 		for i, buf in ipairs(bufs) do
 			local pkt = buf:getUdpPacket()
 			pkt.udp:setSrcPort(1000 + counter)
-			counter = incAndWrap(counter, NUM_FLOWS)
+			counter = incAndWrap(counter, flows)
 		end
 		bufs:offloadUdpChecksums()
 		queue:send(bufs)
@@ -57,7 +53,7 @@ end
 function rxSlave(queue)
 	local bufs = memory.bufArray()
 	ctr = stats:newPktRxCounter(queue, "plain")
-	while dpdk.running(100) do
+	while mg.running(100) do
 		local rx = queue:recv(bufs)
 		for i = 1, rx do
 			local buf = bufs[i]
